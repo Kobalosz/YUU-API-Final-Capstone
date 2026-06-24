7595203c58e125385fbf569dba4213faa2624dba
Kobalosz <imanuel.dartey@gmail.com>
2026-06-24 11:45:50 -0700

    Fix: Autowire CategoriesController and implement CORS, added null check for getById() method.

--- capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,5 +1,6 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,6 +17,9 @@ import java.util.List;
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+@RestController
+@RequestMapping("categories")
+@CrossOrigin
 public class CategoriesController
 {
     private CategoryService categoryService;
@@ -23,6 +27,11 @@ public class CategoriesController
 
 
     // create an Autowired constructor to inject the categoryService and productService
+    @Autowired
+    CategoriesController(CategoryService categoryService, ProductService productService) {
+        this.categoryService = categoryService;
+        this.productService = productService;
+    }
 
     // add the appropriate annotation for a get action
     @GetMapping("")
@@ -37,12 +46,14 @@ public class CategoriesController
     public Category getById(@PathVariable int id)
     {
         // get the category by id
-        return categoryService.getById(id);
+        Category category = categoryService.getById(id);
+        if (category == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
+        return category;
     }
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
-    @GetMapping("{id}/products")
+    @GetMapping("{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
