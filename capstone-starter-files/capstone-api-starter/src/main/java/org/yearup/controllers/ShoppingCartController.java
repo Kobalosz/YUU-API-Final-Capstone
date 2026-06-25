fac67c43107cf4847000a4f0b7b38ca098ecfa5b
Kobalosz <imanuel.dartey@gmail.com>
2026-06-25 13:06:42 -0700

    Feat: Built ShoppingCartController CRUD Request handling methods

--- capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,7 +1,13 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
+import org.yearup.service.ProductService;
 import org.yearup.service.ShoppingCartService;
 import org.yearup.service.UserService;
 
@@ -9,6 +15,9 @@ import java.security.Principal;
 
 // convert this class to a REST controller
 // only logged in users should have access to these actions
+@RestController
+@RequestMapping("cart")
+@PreAuthorize("isAuthenticated()")
 public class ShoppingCartController
 {
     // a shopping cart controller depends on the service layer
@@ -16,8 +25,16 @@ public class ShoppingCartController
     private UserService userService;
 
 
+    @Autowired
+    public ShoppingCartController(ShoppingCartService shoppingCartService, UserService userService, ProductService productService){
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+    }
+
+
 
     // each method in this controller requires a Principal object as a parameter
+    @GetMapping
     public ShoppingCart getCart(Principal principal)
     {
         // get the currently logged in username
@@ -27,20 +44,46 @@ public class ShoppingCartController
         int userId = user.getId();
 
         // use the shoppingCartService to get all items in the cart and return the cart
-        return null;
+        return shoppingCartService.getByUserId(userId);
     }
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15  (15 is the productId to be added)
     // return the updated cart with status 201 Created
+    @PostMapping("products/{productId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ShoppingCart addProduct(Principal principal, @PathVariable("productId") int productId)
+    {
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+
+        return shoppingCartService.addProduct(userId,productId);
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15  (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated; return the cart (200 OK)
-
+    @PutMapping("products/{productId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ShoppingCart updateCart(@PathVariable int productId, @RequestBody ShoppingCartItem cartItem, Principal principal)
+    {
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+        return shoppingCartService.updateProduct(userId,productId,cartItem.getQuantity());
+    }
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart  - return the (now empty) cart so the front end can refresh it (200 OK)
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ShoppingCart deleteCart(Principal principal){
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+        return shoppingCartService.clearCart(userId);
+    }
 
 }
